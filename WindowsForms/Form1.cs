66b0d0ceac71a7fc5d80b795d760c153fbe5b64b
Petrik Lajos Szg <info@petrik.hu>
2018-11-23 14:11:29 +0100

    Ellenőrzéssel

--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -22,8 +22,27 @@ namespace WindowsForms
         {
             string nev = nevTB.Text;
             string jelszo = jelszoTB.Text;
-            DateTime szulDatum = regdatumDTP.Value;
-            using (var conn = new MySqlConnection()) {
+            DateTime regDatum = regdatumDTP.Value;
+            using (var conn = new MySqlConnection("Server=localhost;Database=regisztracio;Uid=root;Pwd=;")) {
+
+                conn.Open();
+
+                var ellenorzes = conn.CreateCommand();
+                ellenorzes.CommandText = "SELECT COUNT(*) FROM felhasznalo WHERE nev = @nev";
+                ellenorzes.Parameters.AddWithValue("@nev",nev);
+                var darab = (int)ellenorzes.ExecuteScalar();
+                if (darab == 0)
+                {
+                    MessageBox.Show("A név már létezik te szerencsétlen -.-!");
+                    return;
+                }
+
+                var command = conn.CreateCommand();
+                command.CommandText = "INSERT INTO `felhasznalo`(`nev`, `jelszo`, `regdatum`) VALUES (@nev,@jelszo,@regDatum)";
+                command.Parameters.AddWithValue("@nev",nev);
+                command.Parameters.AddWithValue("@jelszo", jelszo);
+                command.Parameters.AddWithValue("@regDatum", regDatum);
+                int erintettSorok = command.ExecuteNonQuery();
 
             }
         }
