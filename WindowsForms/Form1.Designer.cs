30c79717e0ae71d633cda591c710305a79cd19d7
Lettike <Lettike@DESKTOP-NE9GGQ3>
2018-11-30 14:58:19 +0100

    Login kész

--- WindowsForms/Form1.Designer.cs
+++ WindowsForms/Form1.Designer.cs
@@ -42,12 +42,22 @@
             this.label7 = new System.Windows.Forms.Label();
             this.jelszoDel = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.regdatumUpdate = new System.Windows.Forms.DateTimePicker();
+            this.nevLogin = new System.Windows.Forms.TextBox();
+            this.jelszoLogin = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // nevTB
             // 
             this.nevTB.Location = new System.Drawing.Point(17, 64);
-            this.nevTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nevTB.Margin = new System.Windows.Forms.Padding(4);
             this.nevTB.Name = "nevTB";
             this.nevTB.Size = new System.Drawing.Size(344, 22);
             this.nevTB.TabIndex = 0;
@@ -55,7 +65,7 @@
             // jelszoTB
             // 
             this.jelszoTB.Location = new System.Drawing.Point(17, 111);
-            this.jelszoTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.jelszoTB.Margin = new System.Windows.Forms.Padding(4);
             this.jelszoTB.Name = "jelszoTB";
             this.jelszoTB.PasswordChar = '*';
             this.jelszoTB.Size = new System.Drawing.Size(344, 22);
@@ -64,17 +74,17 @@
             // regdatumDTP
             // 
             this.regdatumDTP.Location = new System.Drawing.Point(17, 158);
-            this.regdatumDTP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.regdatumDTP.Margin = new System.Windows.Forms.Padding(4);
             this.regdatumDTP.Name = "regdatumDTP";
             this.regdatumDTP.Size = new System.Drawing.Size(344, 22);
             this.regdatumDTP.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(243, 196);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(237, 187);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 28);
+            this.button1.Size = new System.Drawing.Size(123, 37);
             this.button1.TabIndex = 3;
             this.button1.Text = "Regisztráció";
             this.button1.UseVisualStyleBackColor = true;
@@ -110,7 +120,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 235);
+            this.label4.Location = new System.Drawing.Point(394, 13);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(62, 17);
             this.label4.TabIndex = 7;
@@ -127,7 +137,7 @@
             // 
             // nevDel
             // 
-            this.nevDel.Location = new System.Drawing.Point(12, 283);
+            this.nevDel.Location = new System.Drawing.Point(392, 64);
             this.nevDel.Name = "nevDel";
             this.nevDel.Size = new System.Drawing.Size(348, 22);
             this.nevDel.TabIndex = 9;
@@ -152,26 +162,123 @@
             // 
             // jelszoDel
             // 
-            this.jelszoDel.Location = new System.Drawing.Point(13, 333);
+            this.jelszoDel.Location = new System.Drawing.Point(392, 111);
             this.jelszoDel.Name = "jelszoDel";
             this.jelszoDel.Size = new System.Drawing.Size(347, 22);
             this.jelszoDel.TabIndex = 12;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(285, 361);
+            this.button2.Location = new System.Drawing.Point(656, 139);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(83, 31);
             this.button2.TabIndex = 13;
             this.button2.Text = "Törlés";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(392, 180);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "UPDATE";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(392, 43);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 17);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Név:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(390, 90);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 17);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Jelszó:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(393, 205);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 17);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Dátum:";
+            // 
+            // regdatumUpdate
+            // 
+            this.regdatumUpdate.Location = new System.Drawing.Point(395, 235);
+            this.regdatumUpdate.Name = "regdatumUpdate";
+            this.regdatumUpdate.Size = new System.Drawing.Size(347, 22);
+            this.regdatumUpdate.TabIndex = 18;
+            // 
+            // nevLogin
+            // 
+            this.nevLogin.Location = new System.Drawing.Point(13, 283);
+            this.nevLogin.Name = "nevLogin";
+            this.nevLogin.Size = new System.Drawing.Size(348, 22);
+            this.nevLogin.TabIndex = 19;
+            // 
+            // jelszoLogin
+            // 
+            this.jelszoLogin.Location = new System.Drawing.Point(13, 333);
+            this.jelszoLogin.Name = "jelszoLogin";
+            this.jelszoLogin.PasswordChar = '*';
+            this.jelszoLogin.Size = new System.Drawing.Size(347, 22);
+            this.jelszoLogin.TabIndex = 20;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(622, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 37);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Módosítás";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 218);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(51, 17);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "LOGIN";
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(228, 361);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(132, 39);
+            this.login.TabIndex = 23;
+            this.login.Text = "Bejelentkezés";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 524);
+            this.ClientSize = new System.Drawing.Size(755, 404);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.jelszoLogin);
+            this.Controls.Add(this.nevLogin);
+            this.Controls.Add(this.regdatumUpdate);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.jelszoDel);
             this.Controls.Add(this.label7);
@@ -186,7 +293,7 @@
             this.Controls.Add(this.regdatumDTP);
             this.Controls.Add(this.jelszoTB);
             this.Controls.Add(this.nevTB);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -210,6 +317,16 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox jelszoDel;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker regdatumUpdate;
+        private System.Windows.Forms.TextBox nevLogin;
+        private System.Windows.Forms.TextBox jelszoLogin;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button login;
     }
 }
 
--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -13,9 +13,30 @@ namespace WindowsForms
 {
     public partial class Form1 : Form
     {
+        const string AdatLetrehozasSql = "create table if not exists felhasznalo(id integer auto_increment primary key, nev varchar(80) not null unique, jelszo varchar(100) not null, regdatum date not null)";
+
+        MySqlConnection conn;
+
         public Form1()
         {
             InitializeComponent();
+
+            try {
+                conn = new MySqlConnection("Server=localhost;Database=regisztracio;Uid=root;Pwd=;");
+                conn.Open();
+                var letrehozas = conn.CreateCommand();
+                letrehozas.CommandText = AdatLetrehozasSql;
+                letrehozas.ExecuteNonQuery();
+            }
+            catch (MySqlException e) {
+                MessageBox.Show("Adatbázis hiba: " + e);
+            }
+
+            regdatumUpdate.Enabled = false;
+            button3.Enabled = false;
+            nevDel.Enabled = false;
+            jelszoDel.Enabled = false;
+            button2.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -23,14 +44,11 @@ namespace WindowsForms
             string nev = nevTB.Text;
             string jelszo = jelszoTB.Text;
             DateTime regDatum = regdatumDTP.Value;
-            using (var conn = new MySqlConnection("Server=localhost;Database=regisztracio;Uid=root;Pwd=;")) {
-
-                conn.Open();
-
+            
                 var ellenorzes = conn.CreateCommand();
                 ellenorzes.CommandText = "SELECT COUNT(*) FROM felhasznalo WHERE nev = @nev";
                 ellenorzes.Parameters.AddWithValue("@nev",nev);
-                var darab = (int)ellenorzes.ExecuteScalar();
+                var darab = (long)ellenorzes.ExecuteScalar();
                 if (darab != 0)
                 {
                     MessageBox.Show("A név már létezik te szerencsétlen -.-!");
@@ -43,10 +61,6 @@ namespace WindowsForms
                 command.Parameters.AddWithValue("@jelszo", jelszo);
                 command.Parameters.AddWithValue("@regDatum", regDatum);
                 int erintettSorok = command.ExecuteNonQuery();
-
-            }
-
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -54,16 +68,51 @@ namespace WindowsForms
             string nevTorles = nevDel.Text;
             string jelszoTorles = jelszoDel.Text;
             
-            using (var conn = new MySqlConnection("Server=localhost;Database=regisztracio;Uid=root;Pwd=;"))
-            {
-                conn.Open();
-                
-
                 var command = conn.CreateCommand();
-                command.CommandText = "DELETE FROM `felhasznalo` WHERE jelszo = @jelszo";
+                command.CommandText = "DELETE FROM `felhasznalo` WHERE nev = @nev";
                 command.Parameters.AddWithValue("@nev", nevTorles);
                 command.Parameters.AddWithValue("@jelszo", jelszoTorles);
                 command.ExecuteNonQuery();
+            }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string nevBejelntkezes = nevLogin.Text;
+            DateTime regDatumMod = regdatumUpdate.Value;
+            
+                var command = conn.CreateCommand();
+                command.CommandText = "UPDATE `felhasznalo` SET regdatum = @regdatum WHERE nev = @nev";
+                command.Parameters.AddWithValue("@nev",nevBejelntkezes);
+                command.Parameters.AddWithValue("@regDatum", regDatumMod);
+                command.ExecuteNonQuery();
+            }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string nevBejelntkezes = nevLogin.Text;
+            string jelszoBejelentkezes = jelszoLogin.Text;
+            
+            var ellenorzes = conn.CreateCommand();
+            ellenorzes.CommandText = "SELECT COUNT(*) FROM felhasznalo WHERE nev = @nev and jelszo = @jelszo";
+            ellenorzes.Parameters.AddWithValue("@nev", nevBejelntkezes);
+            ellenorzes.Parameters.AddWithValue("@jelszo", jelszoBejelentkezes);
+            var darab = (long)ellenorzes.ExecuteScalar();
+            if (darab == 0)
+            {
+                regdatumUpdate.Enabled = false;
+                button3.Enabled = false;
+                nevDel.Enabled = false;
+                jelszoDel.Enabled = false;
+                button2.Enabled = false;
+                MessageBox.Show("Nem van ilyen felhasználó!");
+                return;
+            }
+            else {
+                button3.Enabled = true;
+                regdatumUpdate.Enabled = true;
+                nevDel.Enabled = true;
+                jelszoDel.Enabled = true;
+                button2.Enabled = true;
             }
         }
     }
